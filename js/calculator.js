3838ac86ea9f9a8ea0feaff0c733b1c43d731f1c
Abrahm-Rollins <abrahmrollins@gmail.com>
2021-01-03 17:01:34 -0800

    javascript additions and php

---
+++ js/calculator.js
@@ -0,0 +1,6 @@
+
+//get starting date
+let startDay = document.getElementById("arrivalDate").value;
+let startTime =  document.getElementById("arrivalTime").value;
+const startStorage = startDay + startTime;
+console.log(startStorage);

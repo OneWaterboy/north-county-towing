3b3ea65e58a97bb0813b0e525e298f4ae3797889
OneWaterboy <abrahmrollins@gmail.com>
2021-02-11 13:15:35 -0800

    fiddling with value outputs
    Cannot get away from NaN when calculating. Need to consult with stack overflow

--- js/calculator.js
+++ js/calculator.js
@@ -27,23 +27,22 @@ let pickUpTime = document.getElementById("outTime").addEventListener("change", f
 
 //Determine Cost Based off of fee rates
 
-let laborHours = document.getElementById("laborCharges").addEventListener("change", function() {
+let laborRate = document.getElementById("laborCharges").addEventListener("change", function() {
                     var input = this.value;
-                    console.log(input);
+                    return input;
                     });
-let laborFees = document.getElementById("laborHoursSelect").addEventListener("change", function() {
+let laborHours = document.getElementById("laborHoursSelect").addEventListener("change", function() {
                     var input = this.value;
-                    if(input === "wspRates"){
-                        console.log(laborHours * 184)
-                    } else if (input === "pvtRates") {
-                        console.log(laborHours * 248)
-                    }
-                });
+                    return input;
+                    });
+
 
 
 function calculateTowingFees () {
     let begin = startDay + startTime;
     let end = pickUpDate + pickUpTime;  // Need To Figure out how to build out a date object and the n subtract to get sotrage hours
+    let impoundCharge = parseInt(laborRate) * parseInt(laborHours);
     console.log(begin);
     console.log(end);
+    console.log(impoundCharge);
 }

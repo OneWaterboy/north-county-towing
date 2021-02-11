6b9164e803c7725eb7d259372dc4233b1894d7e6
OneWaterboy <abrahmrollins@gmail.com>
2021-02-11 11:55:32 -0800

    Created Date Retrival Functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,5 +1,32 @@
 
-//get starting date
-let startDay = document.getElementById("arrivalDate");
+function sayHello () {
+    alert('Hello World');
+}
+
+//Get Starting Date
+let startDay = document.getElementById("arrivalDate").addEventListener("change", function() {
+                    var input = this.value;
+                    console.log(input);
+                    });
 let startTime =  document.getElementById("arrivalTime").value;
+
+//Get Pick Up Date or Estimated Pick Up Date
+
+let pickUpDate = document.getElementById("outDate").addEventListener("change", function() {
+                    var input = this.value;
+                    console.log(input);
+                    });
+let pickUpTime = document.getElementById("outTime").value;
+
+
+//Variables to determine cost calculations
+
 let laborHours = document.getElementById("laborHoursSelect").value;
+let laborFees = document.getElementById("laborHoursSelect").value;
+
+
+function calculateTowingFees () {
+    let begin = startDay;
+    let end = pickUpDate;
+    console.log(end - begin);
+}
